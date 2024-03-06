35ff1cdec8504bfa30221b5469e1ceeced46241a
TooManyFactoryResets <155082829+TooManyFactoryResets@users.noreply.github.com>
2024-03-06 08:36:22 -0600

    Update puzzle.tsx

--- components/puzzle.tsx
+++ components/puzzle.tsx
@@ -1,49 +1,89 @@
 export const solution = {
   id: 15,
   filename: "quickSort",
-  extension: "js",
-  code: `// https://github.com/trekhleb/javascript-algorithms/tree/master/src/algorithms/sorting/quick-sort
-/**
- * @param {*[]} originalArray
- * @return {*[]}
- */
-function quickSort(originalArray) {
-  // Clone original array to prevent it from modification.
-  const array = [...originalArray];
+  extension: "java",
+  code: `// https://www.geeksforgeeks.org/quick-sort
+// Java implementation of QuickSort
+import java.io.*;
 
-  // If array has less than or equal to one elements then it is already sorted.
-  if (array.length <= 1) {
-    return array;
-  }
+class quickSort {
 
-  // Init left and right arrays.
-  const leftArray = [];
-  const rightArray = [];
+	// A utility function to swap two elements
+	static void swap(int[] arr, int i, int j)
+	{
+		int temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
+	}
 
-  // Take the first element of array as a pivot.
-  const pivotElement = array.shift();
-  const centerArray = [pivotElement];
+	// This function takes last element as pivot,
+	// places the pivot element at its correct position
+	// in sorted array, and places all smaller to left
+	// of pivot and all greater elements to right of pivot
+	static int partition(int[] arr, int low, int high)
+	{
+		// Choosing the pivot
+		int pivot = arr[high];
 
-  // Split all array elements between left, center and right arrays.
-  while (array.length) {
-    const currentElement = array.shift();
+		// Index of smaller element and indicates
+		// the right position of pivot found so far
+		int i = (low - 1);
 
-    if (this.comparator.equal(currentElement, pivotElement)) {
-      centerArray.push(currentElement);
-    } else if (currentElement < pivotElement) {
-      leftArray.push(currentElement);
-    } else {
-      rightArray.push(currentElement);
-    }
-  }
+		for (int j = low; j <= high - 1; j++) {
 
-  // Sort left and right arrays.
-  const leftArraySorted = this.sort(leftArray);
-  const rightArraySorted = this.sort(rightArray);
+			// If current element is smaller than the pivot
+			if (arr[j] < pivot) {
 
-  // Let's now join sorted left array with center array and with sorted right array.
-  return leftArraySorted.concat(centerArray, rightArraySorted);
+				// Increment index of smaller element
+				i++;
+				swap(arr, i, j);
+			}
+		}
+		swap(arr, i + 1, high);
+		return (i + 1);
+	}
+
+	// The main function that implements QuickSort
+	// arr[] --> Array to be sorted,
+	// low --> Starting index,
+	// high --> Ending index
+	static void quickSort(int[] arr, int low, int high)
+	{
+		if (low < high) {
+
+			// pi is partitioning index, arr[p]
+			// is now at right place
+			int pi = partition(arr, low, high);
+
+			// Separately sort elements before
+			// partition and after partition
+			quickSort(arr, low, pi - 1);
+			quickSort(arr, pi + 1, high);
+		}
+	}
+	// To print sorted array
+	public static void printArr(int[] arr)
+	{
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
+	}
+
+	// Driver Code
+	public static void main(String[] args)
+	{
+		int[] arr = { 10, 7, 8, 9, 1, 5 };
+		int N = arr.length;
+
+		// Function call
+		quickSort(arr, 0, N - 1);
+		System.out.println("Sorted array:");
+		printArr(arr);
+	}
 }
+
+// This code is contributed by Ayush Choudhary
+// Improved by Ajay Virmoti
 `
 }
 
