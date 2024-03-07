f9e857c817fffd5a9ea76e688f6f5bde47a2b269
TooManyFactoryResets <155082829+TooManyFactoryResets@users.noreply.github.com>
2024-03-06 19:21:42 -0600

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -7,7 +7,6 @@ export default function Document() {
         <meta name="msvalidate.01" content="F7FCF017B4494FA8880AFDF0A157A397" />
         <meta name="google-site-verification" content="BV9zXVsM6TPEh59JTa3-Or0q-FiA_KiA1S4LmY16vlU" />
         <script defer data-domain="debugle.net" src="https://plausible.io/js/plausible.js"></script>
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4146890929235011" crossOrigin="anonymous"></script>
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
